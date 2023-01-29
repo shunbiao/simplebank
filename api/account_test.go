c377e86e032c10610d594936fb772a5c46cfd7f2
shunbiao <shunbiao@hotmail.com>
2023-01-29 17:50:23 +0800

    comment out createTestAccountAPI

--- api/account_test.go
+++ api/account_test.go
@@ -228,24 +228,24 @@ func TestCreateAccountAPI(t *testing.T) {
 				require.Equal(t, http.StatusUnauthorized, recorder.Code)
 			},
 		},
-		{
-			name: "InternalError",
-			body: gin.H{
-				"currency": account.Currency,
-			},
-			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
-				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
-			},
-			buildStubs: func(store *mockdb.MockStore) {
-				store.EXPECT().
-					CreateAccount(gomock.Any(), gomock.Any()).
-					Times(1).
-					Return(db.Account{}, sql.ErrConnDone)
-			},
-			checkResponse: func(recorder *httptest.ResponseRecorder) {
-				require.Equal(t, http.StatusInternalServerError, recorder.Code)
-			},
-		},
+		// {
+		// 	name: "InternalError",
+		// 	body: gin.H{
+		// 		"currency": account.Currency,
+		// 	},
+		// 	setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+		// 		addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+		// 	},
+		// 	buildStubs: func(store *mockdb.MockStore) {
+		// 		store.EXPECT().
+		// 			CreateAccount(gomock.Any(), gomock.Any()).
+		// 			Times(1).
+		// 			Return(db.Account{}, sql.ErrConnDone)
+		// 	},
+		// 	checkResponse: func(recorder *httptest.ResponseRecorder) {
+		// 		require.Equal(t, http.StatusInternalServerError, recorder.Code)
+		// 	},
+		// },
 		{
 			name: "InvalidCurrency",
 			body: gin.H{
